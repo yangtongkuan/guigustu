6a95c1a0714ef78f1eea3e6731a9853bb1dcdbc0
yangtongkuan <yangtongkuan@qq.com>
2019-12-19 18:23:53 +0800

    ## logging

--- spring-boot-03-logging/src/test/java/com/example/SpringBoot03LoggingApplicationTests.java
+++ spring-boot-03-logging/src/test/java/com/example/SpringBoot03LoggingApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringBoot03LoggingApplicationTests {
+public class SpringBoot03LoggingApplicationTests {
+
+    final static Logger logger = LoggerFactory.getLogger(SpringBoot03LoggingApplicationTests.class);
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        logger.trace("这是trace的包");
+        logger.debug("这是debug的...");
+        logger.info("这是info的...");
+        logger.warn("这是warn的...");
+        logger.error("这是error的...");
     }
 
 }
