ca8f51d94dc39d8f53b1af9490d4c427ba78edd3
yangtongkuan <yangtongkuan@qq.com>
2019-12-19 17:37:19 +0800

    ## logging

---
+++ spring-boot-03-logging/src/test/java/com/example/SpringBoot03LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot03LoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
